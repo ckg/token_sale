d9b4e0930e2c64f5ea70277cf80148b428cb8d0b
ckg <gklinavos@gmail.com>
2021-07-04 17:02:54 +0300

    Approve function added

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -11,13 +11,22 @@ contract DappToken{
     //Supply
     uint256 public totalSupply;
 
+    // transfer event from function transfer
     event Transfer(
         address indexed _from,
         address indexed _to,
-        uint256 _value 
+        uint256 _value
+    );
+    // approve event from function approve
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
     );
 
     mapping(address=>uint256) public balanceOf;
+    //allowance
+    mapping(address=>mapping(address=>uint256)) public allowance;
 
     //constructor
     //Read the total number of tokens
@@ -26,23 +35,32 @@ contract DappToken{
         balanceOf[msg.sender] = _initialSupply;
         //Set the total number of tokens
         totalSupply = _initialSupply;
-        
 
     }
 
     // transfer
-    // exception if account doesn't have enough money
-    // return a boolean
     function transfer(address _to, uint256 _value) public returns(bool success) {
+        // exception if account doesn't have enough money
         require(balanceOf[msg.sender] >= _value);
         // transfer the balance
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
         // transfer event
         emit Transfer(msg.sender, _to, _value);
-
+        // return a boolean
         return true;
-    } 
+    }
 
+    //Delegated Transfer
+    //approve
+    function approve(address _spender, uint256 _value) public returns(bool success){
+        //allowance
+        allowance[msg.sender][_spender] = _value;
+        //approve event
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    //transferFrom
 
 }
